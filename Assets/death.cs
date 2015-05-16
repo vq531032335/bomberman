24dd7932950e3f0363fefb2bd01ec160e6b985ec
VicQory <531032335@qq.com>
2015-05-16 17:33:57 +0800

    add findroad

--- Assets/death.cs
+++ Assets/death.cs
@@ -13,13 +13,13 @@ public class death : MonoBehaviour {
 		nowtime=3.0f;
 
 		
-		Destroy(enemy.GetComponent("AI"));
+		//Destroy(enemy.GetComponent("AI"));
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		nowtime-=Time.deltaTime;
+		/*nowtime-=Time.deltaTime;
 		if(nowtime>0&&bombfabs!=null)
 		{
 			Vector3 position=new Vector3(transform.position.x+Random.Range(-0.3f,0.3f),Random.Range(0.0f,0.6f),transform.position.z+Random.Range(-0.3f,0.3f));
@@ -28,6 +28,7 @@ public class death : MonoBehaviour {
 			bbb.setpower(0.1f);
 			bbb.setlefttime(0.01f);
 		}
+		*/
 
 	}
 }
